7f1e2e7ee0015991e044f871fdb70cf7e1b3d594
Keara Berlin <kearaberlin@gmail.com>
2023-03-24 18:35:26 -0500

    format inputs to metric functions

--- summarization.py
+++ summarization.py
@@ -25,7 +25,7 @@ def generate_sentence(description, input_ids, do_sample=False, num_beams=1, top_
     sentence = tokenizer.batch_decode(tokens, skip_special_tokens=True)
     s = "".join(sentence)
     print(f"{description}: {s}")
-    return sentence
+    return s
 
 def calculate_metrics(sentence, reference):
     bert_results = bertscore.compute(predictions=[sentence], references=[reference], lang="en")
@@ -34,28 +34,30 @@ def calculate_metrics(sentence, reference):
 
 for (document, summary) in zip(documents, summaries):
     print(f"\n----------------\n{document}")
+    print(f"Summary: {summary}\n")
+
     input_ids = tokenizer(document, return_tensors="pt").input_ids
     summary_tokens = tokenizer(summary, return_tensors="pt").input_ids
 
     # greedy search (num_beams=1 (default) and do_sample=False)
     # generate up to 30 tokens
     s1 = generate_sentence("greedy search", input_ids, do_sample=False)
-    results = calculate_metrics(s1, summary_tokens)
+    results = calculate_metrics(s1, summary)
 
     # beam search (num_beams > 1, do_sample = False)
     # https://huggingface.co/blog/how-to-generate suggests 5 beams
     s2 = generate_sentence("beam search 5 beams", input_ids, do_sample=False, num_beams=5)
-    results = calculate_metrics(s2, summary_tokens)
+    results = calculate_metrics(s2, summary)
 
     # top-k sampling (do_sample = True, top_p=1)
     # default k is 50
     s3 = generate_sentence("top-k sampling, k=5", input_ids, do_sample=True, top_k=5)
-    results = calculate_metrics(s3, summary_tokens)
+    results = calculate_metrics(s3, summary)
 
     # top-p sampling (do_sample = True, top_p < 1)
     # p = 0.75 from class slides
     s4 = generate_sentence("top-p sampling, p=0.75", input_ids, do_sample=True, top_p=0.75, top_k=0)
-    results = calculate_metrics(s4, summary_tokens)
+    results = calculate_metrics(s4, summary)
 
     with open('output.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
